ce18ad85e834a536e7d629a0f37a07d6b1995a63
jatinojha994ucodiatic <jatin.ojha@codiatic.com>
2024-02-03 11:15:56 +0530

    render-build.sh & render.yaml created for deployment & updated database.yml: render deployment

---
+++ bin/render-build.sh
@@ -0,0 +1,8 @@
+set -o errexit
+
+bundle install
+bundle exec rails assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clobber
+bundle exec rails db:migrate
+bundle exec rails db:seed
